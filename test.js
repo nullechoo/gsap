0b6aa097f5039b653649dbb479fa244c6e3e8671
nullechoo <94823528+nullechoo@users.noreply.github.com>
2023-09-14 18:20:48 +0300

    init

---
+++ test.js
@@ -0,0 +1,75 @@
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".smooth-scroll"),
+    smooth: true,
+    multiplier: 1,
+    reloadOnContextChange: true,
+    lerp: 0.04,
+    direction: "vertical",
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
+    scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+gsap.utils.toArray('section').forEach(function (section) {
+
+    const inner = section.querySelector('.section-inner')
+
+    console.log(section.scrollHeight, section.classList)
+
+    ScrollTrigger.create({
+
+        scroller: '.smooth-scroll',
+        trigger: section,
+        start: section.scrollHeight <= window.innerHeight ? 'top top' : 'bottom bottom',
+        end: '+=100%',
+        pin: inner,
+        pinSpacing: false,
+        pinType: 'transform'
+    })
+
+})
+
+const timeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section-inner',
+        scroller: '.smooth-scroll',
+        scrub: true,
+        start: '50% 0%',
+        endTrigger: '.section-inner',
+        end: 'bottom bottom',
+        anticipatePin: 1,
+        pin: true,
+        pinSpacing: false,
+        pinType: 'transform'
+    }
+});
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
